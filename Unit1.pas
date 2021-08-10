e9cb14db0ee696426ac65a177005912361e52e89
Richard Smythe <82367391+richardsmythe@users.noreply.github.com>
2021-08-10 12:31:21 +0100

    Update Unit1.pas

--- Unit1.pas
+++ Unit1.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, shellapi, Vcl.ComCtrls,
-  Vcl.FileCtrl;
+  Vcl.FileCtrl, FlCtrlEx;
 
 type
   TForm1 = class(TForm)
@@ -14,8 +14,11 @@ type
     Button2: TButton;
     Memo1: TMemo;
     Label1: TLabel;
+    Button3: TButton;
+    Button4: TButton;
     procedure Button2Click(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,6 +34,8 @@ var
 implementation
 
 {$R *.dfm}
+
+uses inputQuery_U;
 { TForm1 }
 
 procedure getSelectedItems(ListBox: TListBox; List: TStringlist);
@@ -64,8 +69,8 @@ begin
     // extract contents using
     extractStrings([], [], pChar(sFiles), Memo1.lines);
     Label1.caption := 'The following files will be printed:';
-    Label1.Visible := true;
-    Memo1.Visible := true;
+    Label1.Visible := True;
+    Memo1.Visible := True;
 
     // send strings to shellexecute
     for i := 0 to ListBox1.Count - 1 do
@@ -78,12 +83,24 @@ begin
     showmessage('Nothing selected');
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.Button3Click(Sender: TObject);
 begin
-  ListBox1.MultiSelect := true;
-  ListBox1.Clear;
-  sDir := 'C:\Users\danpas\Desktop\test\';
+  form2.show;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+var
+  startDir: string;
+  chosenDir: string;
+begin
+  // sDir := 'C:\Users\danpas\Desktop\test\';
+  SelectDirectory('Your caption', startDir, chosenDir);
+  sDir := startDir + chosenDir;
+
+  ListBox1.MultiSelect := True;
+  // ListBox1.Clear;
   ListFiles(sDir, ListBox1.Items);
+
 end;
 
 procedure TForm1.ListFiles(Path: string; FileList: TStrings);
@@ -91,15 +108,16 @@ var
   searchResults: TSearchRec;
   fileAttributes: Integer;
 begin
-
   fileAttributes := faDirectory;
-  if FindFirst(Path + '*.doc*', faAnyFile, searchResults) = 0 then
+
+  if FindFirst(includetrailingpathdelimiter(Path) + '*.doc*', faAnyFile,
+    searchResults) = 0 then
   begin
     repeat
       if (searchResults.Attr <> fileAttributes) then // if not a directory
       begin
         // show full file name used for printing
-        FileList.add(Path + searchResults.Name);
+        FileList.add(includetrailingpathdelimiter(Path) + searchResults.Name);
       end;
     until FindNext(searchResults) <> 0;
     FindClose(searchResults);
